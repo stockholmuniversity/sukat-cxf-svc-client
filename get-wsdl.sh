f6c297fac5fdaaf706ccec62b9bd81137e441474
Jack <jack.enqvist@su.se>
2012-08-14 14:09:49 +0200

    added the stuff

---
+++ get-wsdl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Script to change lpw server from skarp to test or vice versa.
+#
+# Usage:
+# ./change-server.sh skarp OR ./change-server.sh test
+#
+# $Id: change-server.sh 7343 2009-09-22 06:45:27Z hdrys $
+#
+
+SERVER='jqvarlocal.it.su.se'
+
+#space seperated list of wsdl files"
+WSDLFILES="HelloWorld.wsdl Status.wsdl"
+
+for file in $WSDLFILES; do
+  base=`basename $file .wsdl`
+  curl -k -o  src/main/wsdl/$file --negotiate -u : https://$SERVER/$base?wsdl
+  if [ $? -ne 0 ]; then
+    echo "Error: $0 can't download https://$SERVER/$base?wsdl"
+    exit 1
+  fi
+done
