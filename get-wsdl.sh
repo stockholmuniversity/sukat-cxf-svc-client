0db69aad4df6e940fd4c1d885d63d1c811d386bb
jqvar <jan.qvarnstrom@su.se>
2012-12-13 14:55:11 +0100

    Made the script dynamic

--- get-wsdl.sh
+++ get-wsdl.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #
-# Script to change lpw server from skarp to test or vice versa.
-#
-# Usage:
-# ./change-server.sh skarp OR ./change-server.sh test
-#
-# $Id: change-server.sh 7343 2009-09-22 06:45:27Z hdrys $
-#
 
-SERVER='jqvarlocal.it.su.se'
+SERVERURL=`echo "$1" | sed -e "s/\/*$//"`
+if [ -z ${SERVERURL}  ]
+     then 
+     echo "usage: $0 <serverurl>"
+     echo "example: $0 https://myserver.su.se/"
+     exit 10
+ fi
 
 #space seperated list of wsdl files"
-WSDLFILES="CardAdminService.wsdl CardInfoService.wsdl EntitlementService.wsdl ServiceService.wsdl AccountService.wsdl EnrollmentService.wsdl Status.wsdl"
+WSDLFILES="CardAdminService.wsdl CardInfoService.wsdl EntitlementService.wsdl ServiceService.wsdl AccountService.wsdl EnrollmentService.wsdl Status.wsdl WebServiceAdmin.wsdl"
 
 for file in $WSDLFILES; do
   base=`basename $file .wsdl`
-  curl -k -o  src/main/wsdl/$file --negotiate -u : https://$SERVER/$base?wsdl
+  curl -k -o  src/main/wsdl/$file --negotiate -u : $SERVERURL/$base?wsdl
   if [ $? -ne 0 ]; then
-    echo "Error: $0 can't download https://$SERVER/$base?wsdl"
+    echo "Error: $0 can't download $SERVERURL/$base?wsdl"
     exit 1
   fi
 done
